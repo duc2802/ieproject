190ef1649cbcfb9056f2fff5f0927f0ba736ce03
duc2802 <duc2802@dc9de240-c5d8-c47a-9f43-fd357af5fa7e>
2010-10-12 02:58:03 +0000

    present header into XML format... :D

--- trunk/readtextfile/src/demo/input/Main.java
+++ trunk/readtextfile/src/demo/input/Main.java
@@ -4,8 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.StringReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class Main {
 	
@@ -16,7 +26,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String pathFile = "text.txt";
+		//String pathFile = "text.txt";
+		String pathFile = "tagged_headers.txt";		
 		File file = new File(pathFile);	
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -27,8 +38,45 @@ public class Main {
 					dataset.append(line);
 			}
 			String[] header = dataset.toString().split("<NEW_HEADER>");
-			System.out.println(header.length);
-			System.out.println(header[0]);
+		
+			//System.out.println(header.length);
+			//System.out.println(header[0]);
+			int countHeader = 0;
+			for (int i = 0; i < header.length; i++) {
+				StringBuilder headerXML = new StringBuilder();
+				headerXML.append("<NEW_HEADER>");
+				headerXML.append(header[0]);
+				headerXML.append("</NEW_HEADER>");
+				header[i] = headerXML.toString();
+				countHeader++;
+			}
+			System.out.println("Count header : " + countHeader);
+			
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+	        InputSource is = new InputSource();
+	        is.setCharacterStream(new StringReader(header[0]));
+
+	        Document doc = db.parse(is);
+	        NodeList nodes = doc.getElementsByTagName("title");
+	        Element element = (Element) nodes.item(0);
+	        NodeList fstNm = element.getChildNodes();
+	        System.out.println("Title header 1 : "  + ((Node) fstNm.item(0)).getNodeValue());
+	        
+	        NodeList nodes1 = doc.getElementsByTagName("author");
+	        Element element1 = (Element) nodes1.item(0);
+	        NodeList fstNm1 = element1.getChildNodes();
+	        System.out.println("Author header 1 : "  + ((Node) fstNm1.item(0)).getNodeValue());
+	        
+	        NodeList nodes2 = doc.getElementsByTagName("date");
+	        Element element2 = (Element) nodes2.item(0);
+	        NodeList fstNm2 = element2.getChildNodes();
+	        System.out.println("Date Publidh header 1 : "  + ((Node) fstNm2.item(0)).getNodeValue());
+	        
+	        NodeList nodes3 = doc.getElementsByTagName("abstract");
+	        Element element3 = (Element) nodes3.item(0);
+	        NodeList fstNm3 = element3.getChildNodes();
+	        System.out.println("Abstract header 1 : "  + ((Node) fstNm3.item(0)).getNodeValue());
 			
 		} catch (Exception e) {
 			// TODO: handle exception
