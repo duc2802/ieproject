129e7d4026da7df1e0bacc66be3e579a5ffa86b8
duyvo1308 <duyvo1308@dc9de240-c5d8-c47a-9f43-fd357af5fa7e>
2010-10-15 03:58:01 +0000

    add tags

--- trunk/readtextfile/src/utilily/extract/svm/TagConst.java
+++ trunk/readtextfile/src/utilily/extract/svm/TagConst.java
@@ -4,6 +4,46 @@ public class TagConst {
 	
 	public static final String TITLE_TAG_START = "<title>";
 	public static final String TITLE_TAG_END = "</title>";
-	public static final String ABSTRACT_TAG_START = "<abstract>";
 	
+	public static final String ABSTRACT_TAG_START = "<abstract>";
+	public static final String ABSTRACT_TAG_END = "</abstract>";
+	
+	public static final String AUTHOR_TAG_START = "<author>";
+	public static final String AUTHOR_TAG_END = "</author>";
+	
+	public static final String AFFILIATION_TAG_START = "<affiliation>";
+	public static final String AFFILIATION_TAG_END = "</affiliation>";
+	
+	public static final String ADDRESS_TAG_START = "<address>";
+	public static final String ADDRESS_TAG_END = "</address>";
+	
+	public static final String NOTE_TAG_START = "<note>";
+	public static final String NOTE_TAG_END = "</note>";
+	
+	public static final String EMAIL_TAG_START = "<email>";
+	public static final String EMAIL_TAG_END = "</email>";
+	
+	public static final String DATE_TAG_START = "<date>";
+	public static final String DATE_TAG_END = "</date>";
+	
+	public static final String INTRO_TAG_START = "<intro>";
+	public static final String INTRO_TAG_END = "</intro>";
+	
+	public static final String PHONE_TAG_START = "<phone>";
+	public static final String PHONE_TAG_END = "</phone>";
+	
+	public static final String KEYWORD_TAG_START = "<keyword>";
+	public static final String KEYWORD_TAG_END = "</keyword>";
+	
+	public static final String WEB_TAG_START = "<web>";
+	public static final String WEB_TAG_END = "</web>";
+	
+	public static final String DEGREE_TAG_START = "<degree>";
+	public static final String DEGREE_TAG_END = "</degree>";
+	
+	public static final String PUBNUM_TAG_START = "<pubnum>";
+	public static final String PUBNUM_TAG_END = "</pubnum>";
+	
+	public static final String PAGE_TAG_START = "<page>";
+	public static final String PAGE_TAG_END = "</page>";
 }
