f0b019cde60909c67f96611fe4614ef06a1c4c83
duyvo1308 <duyvo1308@dc9de240-c5d8-c47a-9f43-fd357af5fa7e>
2010-10-25 05:46:59 +0000

    add Database code

---
+++ readtextfile/src/core/extract/svm/domaindatabase/LabelWord.java
@@ -0,0 +1,26 @@
+/**
+ * LabelWord.java
+ * @Author : Huynh Minh Duc
+ * 9:29:32 AM 
+ */
+package core.extract.svm.domaindatabase;
+
+/**
+ * @author DuyVo
+ *
+ */
+public class LabelWord {
+	public static final int POSTCODE = 1;
+	public static final int ABSTRACT = 2;
+	public static final int KEYWORD = 3;
+	public static final int PHONE = 4;
+	public static final int MONTH = 5;
+	public static final int ADDRESS = 6;
+	public static final int CITY = 7;
+	public static final int STATE = 8;
+	public static final int COUNTRY = 9;	
+	public static final int NAME = 10;
+	
+	public static final int AFFILIATION = 11;
+	public static final int NOTE = 12;
+}
