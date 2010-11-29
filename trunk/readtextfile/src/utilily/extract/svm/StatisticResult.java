28c37b442f8039d8b8b125ab1ce66f5e218d7527
duyvo1308 <duyvo1308@dc9de240-c5d8-c47a-9f43-fd357af5fa7e>
2010-11-29 07:49:14 +0000

    complete Statistic Result

--- trunk/readtextfile/src/utilily/extract/svm/StatisticResult.java
+++ trunk/readtextfile/src/utilily/extract/svm/StatisticResult.java
@@ -34,6 +34,75 @@ public class StatisticResult {
 	public void setList(ArrayList<LabelMap> list) {
 		this.list = list;
 	}
+	
+	public void returnResultBy (int label) {
+		int totalLineByLabel = 0;
+		int totalCorrectLineByLabel = 0;
+		
+		ArrayList<LabelMap> lableMapArrayList = this.getList();
+		for (LabelMap lm : lableMapArrayList) {		
+			if(lm.getHand() == label){
+				totalLineByLabel++;
+				if(lm.getMachine() == label){
+					totalCorrectLineByLabel++;
+				}
+			}
+		}
+		
+		String labelString;
+		switch (label) {
+		case 1:
+			labelString = "Title";
+			break;
+		case 2:
+			labelString = "Author";
+			break;
+		case 3:
+			labelString = "Affiliation";
+			break;
+		case 4:
+			labelString = "Address";
+			break;
+		case 5:
+			labelString = "Note";
+			break;
+		case 6:
+			labelString = "Email";
+			break;
+		case 7:
+			labelString = "Date";
+			break;
+		case 8:
+			labelString = "Abtract";
+			break;
+		case 9:
+			labelString = "Introduction";
+			break;
+		case 10:
+			labelString = "Phone";
+			break;
+		case 11:
+			labelString = "Keyword";
+			break;
+		case 12:
+			labelString = "Web";
+			break;
+		case 13:
+			labelString = "Degree";
+			break;
+		case 14:
+			labelString = "Pubnum";
+			break;
+		case 15:
+			labelString = "Page";
+			break;	
+		default:
+			labelString = null;
+			break;
+		}
+		
+		System.out.println("Summer "+ labelString +" line predict correct : " + totalCorrectLineByLabel + "/" + totalLineByLabel + " (" + (double)totalCorrectLineByLabel / totalLineByLabel * 100 + "%)");
+	}
 
 	/**
 	 * @param args
@@ -49,7 +118,7 @@ public class StatisticResult {
 		StatisticResult sr = new StatisticResult();
 		sr.buildLableMap(resultFileName, pathTestScaleFile);
 		
-		int totalCorrectLine = 0;		
+		/*int totalCorrectLine = 0;		
 		int totalTitleLine = 0;
 		int totalCorrectTitleLine = 0;
 		
@@ -69,7 +138,22 @@ public class StatisticResult {
 		
 		System.out.println("Summer line predict correct : " + totalCorrectLine + "/" + lengthLine + " (" + (double) totalCorrectLine / lengthLine * 100 + "%)");
 		System.out.println("Summer TITLE line predict correct : " + totalCorrectTitleLine + "/" + totalTitleLine + " (" + (double)totalCorrectTitleLine / totalTitleLine * 100 + "%)");
-		
+		*/
+		sr.returnResultBy(LabelConst.ABSTRACT);
+		sr.returnResultBy(LabelConst.ADDRESS);
+		sr.returnResultBy(LabelConst.AFFILIATION);
+		sr.returnResultBy(LabelConst.AUTHOR);
+		sr.returnResultBy(LabelConst.DATE);
+		sr.returnResultBy(LabelConst.DEGREE);
+		sr.returnResultBy(LabelConst.EMAIL);
+		sr.returnResultBy(LabelConst.INTRODUCTION);
+		sr.returnResultBy(LabelConst.KEYWORD);
+		sr.returnResultBy(LabelConst.NOTE);
+		sr.returnResultBy(LabelConst.PAGE);
+		sr.returnResultBy(LabelConst.PHONE);
+		sr.returnResultBy(LabelConst.PUBNUM);
+		sr.returnResultBy(LabelConst.TITLE);
+		sr.returnResultBy(LabelConst.WEB);
 	}
 
 }
